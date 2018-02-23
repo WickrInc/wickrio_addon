13723c56f2f08bfc037fc74e0174b50af498af52
Paul Cushman <pcushman@wickr.com>
2018-02-23 10:24:24 -0500

    Remove test code [ci skip]

--- services/consolecmd/main.cpp
+++ services/consolecmd/main.cpp
@@ -24,27 +24,6 @@ int main(int argc, char *argv[])
 
     coreapp.setOrganizationName(WBIO_ORGANIZATION);
     coreapp.setApplicationName(WBIO_CONSOLE_TARGET);
-
-
-    QLibrary library("/opt/WickrIODebug/integrations/software/testIntegration/libtestIntegration.so");
-    if (!library.load()) {
-        qDebug() << "Could not load testintegration library";
-    } else {
-        qDebug() << "testintegration library loaded";
-        typedef QString (*TestInputfunction)(QString input);
-        TestInputfunction tf = (TestInputfunction)library.resolve("processInput");
-        if (tf) {
-            QString output = tf("This is a test message");
-            if (output.isEmpty()) {
-                qDebug() << "CONSOLE:received empty string";
-            } else {
-                qDebug() << "CONSOLE:Received:" << output;
-            }
-        }
-    }
-
-
-
     CmdMain cmdmain;
     if (argc > 1) {
         QString commands(argv[1]);
