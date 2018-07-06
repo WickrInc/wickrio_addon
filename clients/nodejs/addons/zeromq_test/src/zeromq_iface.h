35f6debb1718582bfc58c4af79b6cf8dff4acd01
Paul Cushman <pcushman@wickr.com>
2018-07-06 16:32:07 -0400

    Adding the zeromq test program [ci skip]

---
+++ clients/nodejs/addons/zeromq_test/src/zeromq_iface.h
@@ -0,0 +1,35 @@
+#ifndef RABBITMQ_TEST_H
+#define RABBITMQ_TEST_H
+
+#if __cplusplus > 199711L // C++11 or greater
+#include <functional>
+#endif
+
+#include <unistd.h>
+#include "zmq.h"
+
+using namespace std;
+
+class MesgQueueIface
+{
+public:
+    MesgQueueIface(string client) : m_clientName(client) {}
+    ~MesgQueueIface();
+
+    bool init();
+    string sendMessage(string message);
+
+private:
+    string      m_clientName;
+
+    void        *m_zctx = nullptr;
+    void        *m_zTxSocket = nullptr;
+    void        *m_zRxSocket = nullptr;
+
+    string m_responseString;
+
+    string m_requestQName;
+
+};
+
+#endif // RABBITMQ_TEST_H
