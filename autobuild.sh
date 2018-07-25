b6c43581e78444b290ea3429f95528a6b5ae8a0e
Paul Cushman <pcushman@wickr.com>
2018-07-25 10:33:18 -0400

    Fix autobuild, due to move of the addon directory [ci skip]

--- autobuild.sh
+++ autobuild.sh
@@ -316,7 +316,7 @@ make $bldtype.install
 #
 # Build the node addon
 #
-(cd clients/nodejs/addons/wickrio_node.js_addon; rm -rf build; npm install; cmake-js rebuild)
+(cd integrations/nodejs/addon; rm -rf build; npm install; cmake-js rebuild)
 
 #
 # Deploy this thing
