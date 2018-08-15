4b6a4a5a0533030471d669a4c14540376ceda5d6
Aaron <amishiyev@wickr.com>
2018-08-15 18:35:38 -0400

    Add statistics endpoint to web_interface

--- integrations/nodejs/web_interface/server.js
+++ integrations/nodejs/web_interface/server.js
@@ -26,14 +26,13 @@ return new Promise(async (resolve, reject) => {
 app.listen(port, () => {
   console.log('We are live on ' + port);
 });
-var endpoint = "/Apps/" + api_key + "/Messages";
-app.post(endpoint, function(req, res){
-  // console.log('res:',res);
-  // console.log('req.query:',req.query);
+
+var endpoint = "/Apps/" + api_key;
+
+app.post(endpoint + "/Messages", async function(req, res){
   console.log('req.body:',req.body);
   res.send("OK 200");
   res.end();
-  var message = req.body.message;
   var ttl = "", bor = "";
   if(req.body.ttl)
     ttl = req.body.ttl.toString();
@@ -44,16 +43,16 @@ app.post(endpoint, function(req, res){
   for(var i in req.body.users){
   users.push(req.body.users[i].name);
 }
-  // console.log('users:',users);
   if(req.body.attachment){
    var attachment;
    var displayName = "";
    if(req.body.attachment.url){
-    displayName = req.body.attachment.filename;
-    attachment = req.body.attachment.url;
+    attachment = req.body.attachment.url.toString();
+    displayName = req.body.attachment.displayname.toString();
    }
    else{
-    attachment = req.body.attachment.toString();
+    attachment = req.body.attachment.filename.toString();
+    displayName = req.body.attachment.displayname.toString();
    }
    console.log('users:', users);
    console.log('attachment:', attachment);
@@ -61,7 +60,7 @@ app.post(endpoint, function(req, res){
    console.log(addon.cmdSend1to1Attachment(users, attachment, displayName, ttl, bor));
    }
    else{
-   console.log('1to1Message!')
+   var message = req.body.message;
    var csm = addon.cmdSend1to1Message(users, message, ttl , bor);
    console.log(csm);
    }
@@ -73,19 +72,31 @@ app.post(endpoint, function(req, res){
    var displayName = "";
    if(req.body.attachment.url){
     attachment = req.body.attachment.url.toString();
-    displayName = req.body.attachment.filename.toString();
+    displayName = req.body.attachment.displayname.toString();
    }
    else{
-    attachment = req.body.attachment.toString();
+    attachment = req.body.attachment.filename.toString();
+    displayName = req.body.attachment.displayname.toString();
    }
+     console.log('attachment:', attachment);
+     console.log('displayName:', displayName);
      console.log(addon.cmdSendRoomAttachment(vGroupID, attachment, displayName, ttl, bor));
    }
    else{
+     var message = req.body.message;
      console.log(addon.cmdSendRoomMessage(vGroupID, message, ttl, bor));
    }
   }
-
 });
+
+
+app.get(endpoint + "/Statistics", async function(req, res){
+  console.log('req.body:',req.body);
+  var statistics = await addon.cmdGetStatistics();
+  console.log(statistics);
+  res.send(statistics);
+});
+
 }).catch(error => {
   console.log('Error: ', error);
 });
