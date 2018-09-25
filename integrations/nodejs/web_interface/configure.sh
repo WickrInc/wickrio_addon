68cff34dff70748075e5feca76f7555fe3833c85
Aaron <amishiyev@wickr.com>
2018-09-25 18:00:29 -0400

    Make Web Node.js API use basic auth

--- integrations/nodejs/web_interface/configure.sh
+++ integrations/nodejs/web_interface/configure.sh
@@ -37,3 +37,14 @@ echo "prompt: Please enter your client bot's port:"
       echo "Cannot leave client bot's API-Key empty! Please enter a value:"
     fi
   done
+ echo "prompt: Please create an Web API Basic Authorization Token(we recommend an alphanumeric string with at least 24 characters):"
+  while [ -z "$input4" ]
+   do
+    read  input4
+    if [ ! -z "$input4" ]
+     then
+      echo 'BOT_API_AUTH_TOKEN='${input4} >>client_bot_info.txt
+     else
+       echo "Cannot leave Basic Authorization Token empty! Please enter a value:"
+     fi
+   done
