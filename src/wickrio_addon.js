a2cbf765a297b8e81f1e90383b84de66afe5798c
Paul Cushman <paulcus@amazon.com>
2024-09-12 01:45:51 +0000

    fix issue with cmdSetMessageStatus

--- src/wickrio_addon.js
+++ src/wickrio_addon.js
@@ -1944,7 +1944,7 @@ class WickrIOAddon extends EventEmitter {
       action : 'set_message_status',
       message_id : messageID,
       user : user,
-      status : status,
+      status : parseInt(status),
     }
 
     if (statusMessage?.length > 0) {
